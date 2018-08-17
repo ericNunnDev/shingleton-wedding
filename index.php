2121f7d4cfb0e8123f124ba07501af9a4b783f75
lowlyherotech@gmail.com <[lowlyherotech@gmail.com]>
2018-08-17 10:57:45 -0400

    form php

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123906110-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-123906110-1');
+
+    </script>
+
+    <title>Shingleton Wedding|2018</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="styles/styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Header / Home-->
+    <header class="w3-display-container w3-wide bgimg w3-grayscale-min" id="home">
+        <div class="w3-display-middle w3-pale-red w3-center w3-padding" id="box-border">
+            <h1 class="w3-jumbo display-text"><span>Timothy & Jacqueline</span></h1>
+            <h2><b>Are getting married!</b></h2>
+            <h2><b>September 28<sup>th</sup>, 2018</b></h2>
+            <h2 id="countdown"><b></b></h2>
+        </div>
+        <div>
+            <h2 class="w3-display-middle w3-text-white w3-center w3-padding" id="countdown"></h2>
+        </div>
+    </header>
+
+    <!-- Navbar (sticky bottom) -->
+    <nav class="w3-bottom w3-hide-small" id="nav">
+        <div class="w3-bar w3-white w3-center w3-padding w3-opacity-min w3-hover-opacity-off">
+            <a href="#home" style="width:25%" class="w3-bar-item w3-button">Registries</a>
+            <a href="#us" style="width:25%" class="w3-bar-item w3-button">Timothy & Jacqueline</a>
+            <a href="#wedding" style="width:25%" class="w3-bar-item w3-button">Wedding</a>
+            <a href="#rsvp" style="width:25%" class="w3-bar-item w3-button w3-hover-black">RSVP</a>
+        </div>
+    </nav>
+
+    <!-- About -->
+
+    <div class="w3-container w3-padding-64 w3-pale-red w3-grayscale-min" id="us">
+        <div class="w3-content">
+            <p class="w3-center"><b>Hi Everyone!</b><br><i>First off, we'd like to say that your presence at our wedding is the greatest gift we could ask for. The time, effort and expense of traveling to be with us is something that we deeply appreciate in our hearts; and we are so thrilled to celebrate this happy occasion with you. Thanks again and we can't wait to see you at the wedding (or earlier!).<br>Love,<br> Timothy & Jacqueline</i></p>
+            <figure class="w3-center">
+                <img class="w3-image w3-mobile w3-grayscale-min border-img" id="main-picture" src="img/tim_and_jackie.jpg">
+            </figure>
+            <h1 class="w3-center w3-text-grey"><b>Our Story</b></h1>
+            <p><i>Our love story began back in <date>January, 2013</date>. While Jacqueline was attending Central Michigan University, she was introduced to Timothy through his sister, Alexandrea. It took very little time for them to recognize a strong connection between each other. The night they met was all that it took, and the rest is history.</i></p><br>
+            <p><i>Years of memories later, and the bride and groom's relationship has endured many triumphs and tribulations. The greatest highlights include the birth of their beautiful daughters, Charlotte and Violette. As well as a new home, with 3 fur babies Prince, Maci, and Maverick. They cannot wait to make official what they have known all along:</i><b> that they are made for each other.</b></p><br>
+
+            <p class="w3-center"><a href="#wedding" class="w3-button w3-black w3-round w3-padding-large w3-large">Wedding Details</a></p>
+        </div>
+    </div>
+
+    <!-- Background photo -->
+
+    <!-- START WEDDING SECTION -->
+
+    <div class="w3-container w3-text-white w3-center bgimg2">
+        <div>
+            <h1 class="w3-jumbo wedding w3-display-top"><span>The Wedding</span></h1><br>
+            <h3>Timothy & Jacqueline</h3>
+            <p><img class="w3-image w3-mobile w3-grayscale-min border-img" id="wed-tj-img" src="img/tj-pic.jpg" alt="Timothy and Jacqueline Headshot"></p>
+        </div>
+
+        <!-- START WEDDING PARTY SECTION -->
+        <div class="w3-display-bottom">
+            <h2 class="w3-xxlarge"><span>The Wedding Party</span></h2>
+            <div class="flex-container-1">
+                <div class="best-man">
+                    <h3>Best Man</h3>
+                    <img class="w3-image w3-mobile w3-grayscale-min border-img" id="eric" src="img/groomsmen_eric.jpg" alt="Best Man Eric Picture">
+                    <figcaption class="w3-pale-red border-shadow" id="best-m-bio">
+                        <h3>Eric</h3>Best friend and Brother for 23 years.</figcaption>
+                </div>
+                <div class="maid-of-honor">
+                    <h3>Maid of Honor</h3>
+                    <img class="w3-image w3-mobile w3-grayscale-min border-img" id="alexandrea" src="img/bridesmaid_alex.jpg" alt="Maid of Honor Alexandria">
+                    <figcaption class="w3-pale-red border-shadow" id="moh-bio">
+                        <h3>Alexandrea</h3>Best friend for 7 years, roommate,<br> and soon-to-be Sister-in-Law</figcaption>
+                </div>
+            </div>
+
+            <!-- END BEST MAN AND MAID OF HONOR ROW-->
+
+            <!-- START WEDDING PARTY-->
+            <h3>Groomsmen</h3>
+            <div class="flex-container-2">
+                <div class="party">
+                    <img class="w3-image w3-grayscale-min border-img" src="img/groomsmen_garrett.jpg" alt="Groomsmen Garrett">
+                    <figcaption class="w3-pale-red w3-padding border-shadow">
+                        <h3>Garrett</h3>1/3 of the "Three Musketers".<br> Best friend since Kindergarten.</figcaption>
+                </div>
+                <div class="party">
+                    <img class="w3-image sm-img w3-grayscale-min border-img" src="img/groomsmen_britton.jpg" alt="Groomsmen Britton">
+                    <figcaption class="w3-pale-red w3-padding border-shadow">
+                        <h3>Britton</h3>1/3 of the "Three Musketers".<br> Best friend since Kindergarten. </figcaption>
+                </div>
+                <div class="party">
+                    <img class="w3-image w3-grayscale-min border-img" src="img/groomsmen_taurrek.jpg" alt="Groomsmen Taurrekk">
+                    <figcaption class="w3-pale-red w3-padding border-shadow">
+                        <h3>Taurrek</h3>Close friend for 10 years.</figcaption>
+                </div>
+            </div>
+            <!-- Jackie's wedding party -->
+
+            <h3>Bridesmaids</h3>
+            <div class="flex-container-2">
+                <div class="party">
+                    <img class="w3-image w3-grayscale-min border-img" id="jamie" src="img/bridesmaid_jamie.jpg" alt="Matron of Honor Jamie">
+                    <figcaption class="w3-pale-red w3-center w3-padding border-shadow">
+                        <h3>Jamie</h3>Close friend & <br> cousin for a lifetime.</figcaption>
+                </div>
+                <div class="party">
+                    <img class="w3-image sm-img w3-grayscale-min border-img" id="lauren" src="img/bridesmaid_lauren.jpg" alt="Bridesmaid Lauren">
+                    <figcaption class="w3-pale-red w3-padding border-shadow">
+                        <h3>Lauren</h3>Best friend since Kindergarten.</figcaption>
+                </div>
+                <div class="party">
+                    <img class="w3-image sm-img w3-grayscale-min border-img" id="kaci" src="img/bridesmaid_kaci.jpg" alt="Bridesmaid Kaci">
+                    <figcaption class="w3-pale-red w3-padding border-shadow">
+                        <h3>Kaci</h3>Close friend for 14 years.</figcaption>
+                </div>
+            </div>
+        </div>
+        <!-- END WEDDING PARTY -->
+
+
+        <!-- Wedding information -->
+        <div class="w3-container w3-margin w3-pale-red w3-grayscale-min w3-center" id="wedding">
+            <h1 class="w3-text-grey"><b>THE WEDDING</b></h1>
+            <img class="w3-mobile w3-grayscale-min border-img" src="img/gardens.jpg">
+            <hr>
+            <div class="w3-row">
+                <div class="w3-half">
+                    <h2>When</h2>
+                    <p>Friday,
+                        <date>September 28<sup>th</sup>, 2018</date>
+                    </p>
+                    <p>Wedding Ceremony - 5:15 PM EST</p>
+                </div>
+                <div class="w3-half">
+                    <h2>Where</h2>
+                    <p>Taylor Conservatory & Botanical Gardens</p>
+                    <p>22314 Northline Rd. Taylor, MI 48180</p>
+                    <p>Visit the Venue <a id="venue" href="https://taylorconservatory.org/" target="_blank">here</a>.</p>
+                </div>
+            </div>
+            <!-- End WEDDING ROW -->
+        </div>
+
+        <!-- START MAP-->
+        <div class="w3-mobile" id="map"></div>
+
+        <!-- RSVP section -->
+        <div class="w3-container w3-padding-64 w3-pale-red w3-center w3-wide" id="rsvp">
+            <h1>HOPE YOU CAN MAKE IT!</h1>
+            <p class="w3-large">Kindly Respond By August, 31<sup>st</sup>, 2018</p>
+            <p class="w3-xlarge">
+                <a href="#rsvp"><button id="rsvp" onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-round w3-red w3-opacity w3-hover-opacity-off" style="padding:8px 60px">RSVP</button></a>
+            </p>
+        </div>
+
+        <!-- RSVP modal -->
+        <div id="id01" class="w3-modal">
+            <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="padding:32px;max-width:600px">
+                <div class="w3-container w3-white w3-center">
+                    <h1 class="w3-wide">CAN YOU COME?</h1>
+                    <p>We really hope you can make it.</p>
+
+                    <!-- START FORM -->
+
+                    <?php
+       $name = $_POST['name'];
+       $guests = $_POST['num-guests'];
+       $email = $_POST['email'];
+       $song = $_POST['song'];
+       $from = 'From: My Contact Form';
+       $to = 'lowlyherotech@gmail.com';
+       $subject = 'RSVP';
+
+       $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+       if ($_POST['submit']) {
+           if (mail ($to, $subject, $body, $from)) {
+           echo '<p>Message Sent Successfully!</p>';
+           } else {
+           echo '<p>Ah! Try again, please?</p>';
+           }
+       }
+    ?>
+
+                        <form action="index.php" method="POST">
+
+                            <input class="w3-input w3-border" type="text" placeholder="Name(s)" name="name" required>
+                            <input class="w3-input w3-border w3-margin-top" type="number" placeholder="Number of Guests" name="num-guests" required>
+                            <input class="w3-input w3-border w3-margin-top" type="email" placeholder="Email address [ex. myEmailAddress@myemail.com]" name="email">
+                            <input class="w3-input w3-border w3-margin-top" type="text" placeholder="Have a song you want to hear at the wedding? Enter requests here!" name="song">
+
+                        </form>
+
+                        <!--END FORM -->
+
+                        <p><i>Sincerely, Timothy & Jacqueline</i></p>
+                        <div class="w3-row">
+                            <div class="w3-half">
+                                <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-block w3-green">Going</button>
+                            </div>
+                            <div class="w3-half">
+
+                                <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-block w3-red">Can't come</button>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <footer class="w3-center" id="footer">
+        <p class="w3-border-bottom" id="contact-info">Contact Timothy and Jacqueline</p>
+        <p> 1-734-837-1333</p>
+        <p>1-616-841-0348</p>
+        <p><a id="contact-email" href="#">duran2jm@cmich.edu</a></p>
+        <p>Powered by <a href="https://www.facebook.com/LowlyHero/" target="_blank"><img id="lowlylogo" src="img/logo_2.png" alt="Lowly Hero Web Development & Technology Co." target="_blank"></a></p>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="/scripts/map.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARSmdvIbD0A4Sm12dYuZgs8YVF8kXKX6s&callback=initMap" async defer></script>
+    <script src="scripts/timer.js"></script>
+    <script src="/scripts/animations.js"></script>
+</body>
+
+</html>
