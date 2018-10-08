c0841e4ca5bab25cf7dfa9e4eb2c54d38d837721
[lowlyHero] <41123891+lowlyHero@users.noreply.github.com>
2018-10-08 15:07:41 -0400

    replaced RSVP section with Photo section since wedding date has passed, created alert for users until photos are back from photographer and can be uploaded

---
+++ scripts/photos.js
@@ -0,0 +1,3 @@
+    document.getElementById('photos').onclick = function () {
+        alert('Sorry! Our photos are still being perfected! Please come back soon to see pictures of our wedding night!');
+    }
